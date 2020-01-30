dc0785bacf1502e101326ac5c61c160f536d7191
Caden <cadence1547@gmail.com>
2020-01-29 22:50:10 -0800

    pop out an img

--- js-practics/js-game/js-game-challenge.js
+++ js-practics/js-game/js-game-challenge.js
@@ -33,7 +33,7 @@ function checkMyGuess(){
     if(userGuess===answer){
         returnResult.textContent="Bingle! You guess right";
         hint.textContent=" ";
-        
+        showImg();
         
     }else if(userGuess<answer){
         returnResult.textContent="Sorry! You are wrong";
@@ -54,5 +54,18 @@ function checkMyGuess(){
  hint.style.fontWeight="bold";
 userSubmit.addEventListener("click", checkMyGuess);
 
+// show a img if a user guest guessed right
+function showImg() {
+    let src = "https://lh5.googleusercontent.com/proxy/SEy-yxUJn0F3dUjIKI_D6D2HNDxRQJvbCqM_mvD-z6bkRsY70zWNUPj0bWGFSmkx0FYGcFeqP5H2nlBTKsBtO9Ck4gCk68ZmLBwLBDDCayTm3f4Ga3FUjIfZtpDpdMPU0ZgnybdTVlE";
+    image("https://lh5.googleusercontent.com/proxy/SEy-yxUJn0F3dUjIKI_D6D2HNDxRQJvbCqM_mvD-z6bkRsY70zWNUPj0bWGFSmkx0FYGcFeqP5H2nlBTKsBtO9Ck4gCk68ZmLBwLBDDCayTm3f4Ga3FUjIfZtpDpdMPU0ZgnybdTVlE", 600,300, "congrat");
+}
 
 
+function image(src, width, height, alt) {
+    let img = document.createElement("img");
+    img.src = src;
+    img.width = width;
+    img.height = height;
+    img.alt = alt;
+    document.body.appendChild(img);
+}
